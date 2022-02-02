c6fa4c94fdb1eb0cf0623187c4807e33e620a168
GR <yule.gpark@gmail.com>
2022-02-02 22:24:58 +0900

    2, 3단

---
+++ src/Multiplication_table.java
@@ -0,0 +1,14 @@
+
+public class Multiplication_table {
+	public static void main(String[] args) {
+		// 2단
+		for(int i=1; i<=9; ++i) { // java의 for문은 처음, 끝이 아주 직관적이다.
+			System.out.println(2*i);
+		}
+		// 3단
+		for(int i=1; i<=9; ++i) {
+			System.out.println(3*i);
+		}
+	}
+
+}
